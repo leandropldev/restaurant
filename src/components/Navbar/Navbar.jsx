269d0aa6ffce0af22d81dc009bca56dab83861cc
Leandro Reis <developer.reis@gmail.com>
2023-04-22 08:00:05 +0100

    chefs word

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,14 +31,14 @@ const Navbar = () => {
         
         {toggleMenu && (
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'> 
-          <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
-          <ul className='app__navbar-smallscreen_links'>
-            <li className='p__opensans'><a href='#home' onClick={() => setToggleMenu(false)}>Home</a></li>
-            <li className='p__opensans'><a href='#about' onClick={() => setToggleMenu(false)}>About</a></li>
-            <li className='p__opensans'><a href='#menu' onClick={() => setToggleMenu(false)}>Menu</a></li>
-            <li className='p__opensans'><a href='#awards' onClick={() => setToggleMenu(false)}>Awards</a></li>
-            <li className='p__opensans'><a href='#contact' onClick={() => setToggleMenu(false)}>Contact</a></li>
-          </ul>
+            <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
+            <ul className='app__navbar-smallscreen_links'>
+              <li className='p__opensans'><a href='#home' onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li className='p__opensans'><a href='#about' onClick={() => setToggleMenu(false)}>About</a></li>
+              <li className='p__opensans'><a href='#menu' onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li className='p__opensans'><a href='#awards' onClick={() => setToggleMenu(false)}>Awards</a></li>
+              <li className='p__opensans'><a href='#contact' onClick={() => setToggleMenu(false)}>Contact</a></li>
+            </ul>
           </div>
         )}
         
